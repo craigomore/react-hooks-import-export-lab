5b752e5981cf81ccfcf72d953393d24a8c259ef3
Craig Omore <craiggomore@gmail.com>
2025-10-22 17:26:32 +0300

    doing assingments

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React from 'react';
 
-function NavBar() {
+const NavBar = () => {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
   );
-}
+};
+
+export default NavBar; // Default export
